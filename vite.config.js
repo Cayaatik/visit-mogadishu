840e636ab3f47cfb69e453632dfd1306ec9ed488
Atika Isse Ali <atikaisseali6442@gmail.com>
2026-02-27 20:47:44 +0300

    fix  vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/visit-mogadishu/',
-  // <--- ADD THIS LINE EXACTLY LIKE THIS
+  base: '/visit-mogadishu/', // This tells GitHub where your project lives
 })
